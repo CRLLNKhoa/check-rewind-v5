9f5b4ca8f899bebeb100ca2254e1f7c626e68a99
Luong Khoa <lnkhoa12052000@gmail.com>
2024-04-16 21:40:22 +0700

    fix seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,29 @@ const rubik = Rubik({
   display: 'swap',
 })
 
+export const metadata: Metadata = {
+  title: "Check Cost DBG",
+  description: "Website Check Cost coded by Carolo Lương Khoa",
+  referrer: "origin-when-cross-origin",
+  keywords: ["Lương Khoa", "Carolo Lương Khoa", "Day bygone","Check Cost", "Check Rewind"],
+  authors: [{ name: "Carolo Lương Khoa" }],
+  creator: "Carolo Lương Khoa",
+  publisher: "Carolo Lương Khoa",
+  openGraph: {
+    title: "Check Cost DBG",
+    description: "Website Check Cost coded by Carolo Lương Khoa",
+    url: "https://check-rewind.vercel.app",
+    siteName: "Check rewind",
+    images: [
+      {
+        url: "https://i.ibb.co/bPt4cgf/seo.png", // Must be an absolute URL
+      },
+    ],
+    locale: "vi",
+    type: "website",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
